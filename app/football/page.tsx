7973951b8f68d9ad3089c166e67da1a7919c8b79
aisss666 <farizibrapahlawan@gmail.com>
2025-07-22 17:51:09 +0700

    bola

---
+++ app/football/page.tsx
@@ -0,0 +1,7 @@
+export default function FootballPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-900">
+      <h1>Football</h1>
+    </div>
+  );
+}
